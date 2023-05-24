d8a34b6d8ce469a3823e16ec1306a8afd65bd701
Tyler Smith <tyler.smith.la@gmail.com>
2023-05-24 13:46:53 -0400

    ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,9 @@ sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
 sudo apt-get install build-essential
 sudo apt-get install cmake
 
+git clone git@github.com:micropython/micropython.git
+
+cd micropython/myp-cross
+make
+
 echo "DONE!"
