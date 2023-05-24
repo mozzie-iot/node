0e89603b90ac29b4d07ce974e485da7eb2e9864c
Tyler Smith <tyler.smith.la@gmail.com>
2023-05-24 13:55:52 -0400

    ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,19 @@ sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
 sudo apt-get install build-essential
 sudo apt-get install cmake
 
-git clone https://github.com/micropython/micropython.git
+if ! git clone https://github.com/micropython/micropython.git ; then 
+    printf "Failed to clone micropython repo"
+    exit 0
+fi
 
-cd micropython/myp-cross
-make
+if ! cd micropython/mpy-cross ; then
+    printf "No directory: micropython/mpy-cross"
+    exit 0
+fi
+
+if ! make ; then 
+    printf "Failed to make in 'micropython/mpy-cross'"
+    exit 0 
+fi
 
 echo "DONE!"
