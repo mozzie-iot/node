65b2a0746e25715658d70f01d794bebaba09d0b9
Tyler Smith <tyler.smith.la@gmail.com>
2023-05-24 14:09:09 -0400

    ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,32 +6,32 @@ sudo apt-get install cmake
 
 if ! git clone https://github.com/micropython/micropython.git ; then 
     printf "Failed to clone micropython repo"
-    exit 0
+    exit 1
 fi
 
 if ! cd micropython/mpy-cross ; then
     printf "No directory: micropython/mpy-cross"
-    exit 0
+    exit 1
 fi
 
 if ! make ; then 
     printf "Failed to make in 'micropython/mpy-cross'"
-    exit 0 
+    exit 1 
 fi
 
 if ! cd ~ && git clone -b v4.0.2 --recursive https://github.com/espressif/esp-idf.git ; then 
     printf "Failed to clone https://github.com/espressif/esp-idf.git"
-    exit 0
+    exit 1
 fi
 
-if ! ./esp-idf/install.sh ; then 
+if ! ~/esp-idf/install.sh ; then 
     printf "Failed to run script: ./esp-idf/install.sh"
-    exit 0
+    exit 1
 fi
 
-if ! source esp-idf/export.sh ; then 
+if ! source ~/esp-idf/export.sh ; then 
     printf "Failed to source script: ./esp-idf/export.sh"
-    exit 0
+    exit 1
 fi
 
 echo "DONE!"
