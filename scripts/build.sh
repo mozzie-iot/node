59e08448a125321d0fbe10bde5f246318c6f4f2f
Tyler Smith <tyler.smith.la@gmail.com>
2023-05-24 15:33:16 -0400

    ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,46 +1,48 @@
 #!/bin/bash
 
-sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
-sudo apt-get install build-essential
-sudo apt-get install cmake
+ls
 
-if ! git clone https://github.com/micropython/micropython.git ; then 
-    printf "Failed to clone micropython repo"
-    exit 1
-fi
+# sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
+# sudo apt-get install build-essential
+# sudo apt-get install cmake
 
-if ! cd micropython/mpy-cross ; then
-    printf "No directory: micropython/mpy-cross"
-    exit 1
-fi
+# if ! git clone https://github.com/micropython/micropython.git ; then 
+#     printf "Failed to clone micropython repo"
+#     exit 1
+# fi
 
-if ! make ; then 
-    printf "Failed to make in 'micropython/mpy-cross'"
-    exit 1 
-fi
+# if ! cd micropython/mpy-cross ; then
+#     printf "No directory: micropython/mpy-cross"
+#     exit 1
+# fi
 
-cd ../../
+# if ! make ; then 
+#     printf "Failed to make in 'micropython/mpy-cross'"
+#     exit 1 
+# fi
 
-if ! git clone -b v4.0.2 --recursive https://github.com/espressif/esp-idf.git ; then 
-    printf "Failed to clone https://github.com/espressif/esp-idf.git"
-    exit 1
-fi
+# cd ../../
 
-if ! ./esp-idf/install.sh ; then 
-    printf "Failed to run script: ./esp-idf/install.sh"
-    exit 1
-fi
+# if ! git clone -b v4.0.2 --recursive https://github.com/espressif/esp-idf.git ; then 
+#     printf "Failed to clone https://github.com/espressif/esp-idf.git"
+#     exit 1
+# fi
 
-if ! source ./esp-idf/export.sh ; then 
-    printf "Failed to source script: ./esp-idf/export.sh"
-    exit 1
-fi
+# if ! ./esp-idf/install.sh ; then 
+#     printf "Failed to run script: ./esp-idf/install.sh"
+#     exit 1
+# fi
 
-cd micropython/ports/esp32
+# if ! source ./esp-idf/export.sh ; then 
+#     printf "Failed to source script: ./esp-idf/export.sh"
+#     exit 1
+# fi
 
-if ! make submodules ; then 
-    printf "Failed to make submodules in 'micropython/ports/esp32'"
-    exit 1
-fi
+# cd micropython/ports/esp32
+
+# if ! make submodules ; then 
+#     printf "Failed to make submodules in 'micropython/ports/esp32'"
+#     exit 1
+# fi
 
 echo "DONE!"
